918ddfb1aba3b6a8773ab1552c4cec82538e75d8
Mr Whipp <dwhipp88@gmail.com>
2021-09-03 02:55:23 -0600

    added express server with npm start and created a catch all route

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+// Default response for any other request (Not Found)
+// IT MUST BE THE LAST ROUTE OR IT WILL BREAK THE CODE
+// BY NOT ALLOWING THE OTHER ROUTES TO FIRE
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// starts Express.js server on port 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
