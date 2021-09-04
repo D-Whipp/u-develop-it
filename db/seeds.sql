6ca86663709f391612b120f8dc17925dd714c528
Mr Whipp <dwhipp88@gmail.com>
2021-09-04 02:44:25 -0600

    added parties table

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,6 +14,8 @@ VALUES
     ('Moash', 'Traitor', 0),
     ('Taravangian', 'Manic', 0);
 
-    UPDATE candidates
-    SET industry_connected = 0
-    WHERE id = 2;
+INSERT INTO parties (name, description)
+VALUES
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
+  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
